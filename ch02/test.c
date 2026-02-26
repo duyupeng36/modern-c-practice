86e7b81fa3bfe12665f62066bfcecca2f94461b9
duyupeng <duyupeng36@foxmail.com>
2026-02-26 23:45:54 +0800

    Destription: 类型转换
    Change-Id: Idded8e219117b23efb9b0f86fe86415356f90472

--- ch02/test.c
+++ ch02/test.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
-
+#include <stdbool.h>
 
 int main()
 {
-    printf("%lf", M_PI);
+    unsigned int x = 20u;
+    int y = 30;
+    bool flag = x - y < 0;
+    // 期望输出结果是 `true`
+    printf("%s\n", flag ? "true":"false"); 
 }
